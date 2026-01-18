8b0bcff179f6802151596bc5404a6527994ae9ae
Abdulappa <abhimh033@gmail.com>
2026-01-19 03:26:01 +0530

    Shell Program added

---
+++ Program-8/shell.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "--------------------------------"
+echo "Welcome to Docker Offline Demo..!"
+echo "This Script is Running inside a Container..!!"
+echo "--------------------------------"
